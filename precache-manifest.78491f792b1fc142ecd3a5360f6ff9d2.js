bc60426a3243e973c2752191237ac618f79fcffa
akshatsinghkaushik <37283605+akshatsinghkaushik@users.noreply.github.com>
2020-07-31 07:42:40 +0000

    Deploying to gh-pages from  @ d1777f3bc09ed30b2419ac6d2826b5ae38b3a33d 🚀

---
+++ precache-manifest.78491f792b1fc142ecd3a5360f6ff9d2.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "164aa1a2bf72a90bb6cce9316bfda1b0",
+    "url": "/ReactPractice_Widgets-WikipediaAPI/index.html"
+  },
+  {
+    "revision": "7646ed835f645357f787",
+    "url": "/ReactPractice_Widgets-WikipediaAPI/static/css/2.b1eb0e80.chunk.css"
+  },
+  {
+    "revision": "7646ed835f645357f787",
+    "url": "/ReactPractice_Widgets-WikipediaAPI/static/js/2.5ec85d1d.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/ReactPractice_Widgets-WikipediaAPI/static/js/2.5ec85d1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7afa1e2674470b549111",
+    "url": "/ReactPractice_Widgets-WikipediaAPI/static/js/main.257e7c2e.chunk.js"
+  },
+  {
+    "revision": "690d1f11e3bfe437e89c",
+    "url": "/ReactPractice_Widgets-WikipediaAPI/static/js/runtime-main.c9c7a5c3.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/ReactPractice_Widgets-WikipediaAPI/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/ReactPractice_Widgets-WikipediaAPI/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/ReactPractice_Widgets-WikipediaAPI/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/ReactPractice_Widgets-WikipediaAPI/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/ReactPractice_Widgets-WikipediaAPI/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/ReactPractice_Widgets-WikipediaAPI/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/ReactPractice_Widgets-WikipediaAPI/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/ReactPractice_Widgets-WikipediaAPI/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/ReactPractice_Widgets-WikipediaAPI/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/ReactPractice_Widgets-WikipediaAPI/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/ReactPractice_Widgets-WikipediaAPI/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/ReactPractice_Widgets-WikipediaAPI/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/ReactPractice_Widgets-WikipediaAPI/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/ReactPractice_Widgets-WikipediaAPI/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/ReactPractice_Widgets-WikipediaAPI/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/ReactPractice_Widgets-WikipediaAPI/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
