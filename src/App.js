d1777f3bc09ed30b2419ac6d2826b5ae38b3a33d
akshatsinghkaushik <akshatsingh99@gmail.com>
2020-07-31 17:41:01 +1000

    Commented out code which created Warnings after compile

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import Accordion from './components/Accordion';
+//import Accordion from './components/Accordion';
 import Search from './components/Search';
 
-const items = [
-  {
-    title: 'What is React?',
-    content: 'React is a frontend javascript framework',
-  },
-  {
-    title: 'Why use React?',
-    content: 'React is a favorite JS library among engineers',
-  },
-  {
-    title: 'How do you use React?',
-    content: 'You use React by using components',
-  },
-];
+// const items = [
+//   {
+//     title: 'What is React?',
+//     content: 'React is a frontend javascript framework',
+//   },
+//   {
+//     title: 'Why use React?',
+//     content: 'React is a favorite JS library among engineers',
+//   },
+//   {
+//     title: 'How do you use React?',
+//     content: 'You use React by using components',
+//   },
+// ];
 
 export default () => {
   return (
